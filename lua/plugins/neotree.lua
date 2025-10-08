bfdb419505dc9d029a086a17808fff5c3cc460c8
jamesballaro <jamesalexballaro@gmail.com>
2025-10-08 10:31:51 +0100

    Added toggle dotfiles keymap

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -6,12 +6,14 @@ return {
 			enable_diagnostics = true,
 			window = {
 				width = 30,
-				-- v
+				mappings = {
+					["<S-h>"] = "toggle_hidden",
+				},
 			},
 			filesystem = {
 				filtered_items = {
 					visible = false,
-					hide_dotfiles = false,
+					hide_dotfiles = true,
 					hide_gitignored = false,
 				},
 			},
@@ -38,6 +40,19 @@ return {
 					require("neo-tree.command").execute({ size = true })
 				end,
 				desc = "Change filetree width",
+			},
+			-- Toggle  dotfiles
+			{
+				"<S-h>",
+				function()
+					local manager = require("neo-tree.sources.manager")
+					local renderer = require("neo-tree.ui.renderer")
+					local state = manager.get_state("filesystem")
+					local filters = state.filtered_items
+					filters.hide_dotfiles = not filters.hide_dotfiles
+					renderer.redraw(state)
+				end,
+				desc = "Toggle  dotfiles",
 			},
 		},
 	},
